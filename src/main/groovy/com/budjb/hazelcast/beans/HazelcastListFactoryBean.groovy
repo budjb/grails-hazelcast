bd96ada3900f1fc8c27a3d020d110ed5cb09b8ae
aaron-brown <aaron@aarocode.com>
2017-02-17 11:28:23 -0600

    Added HazelcastListFactoryBean.
    Updated HazelcastMapFactoryBeanSpec so that the
    HazelcastInstance is cleaned up; not doing this
    causes testing side-effects that fail another
    test because the instance was already created.

---
+++ src/main/groovy/com/budjb/hazelcast/beans/HazelcastListFactoryBean.groovy
@@ -0,0 +1,67 @@
+package com.budjb.hazelcast.beans
+
+import com.budjb.hazelcast.HazelcastInstanceService
+import org.springframework.beans.factory.FactoryBean
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Autowired
+
+/**
+ * A {@link FactoryBean} that exposes a distributed Hazelcast list as a Spring bean.
+ *
+ * @param < T >
+ */
+class HazelcastListFactoryBean<T> implements FactoryBean<List<T>>, InitializingBean {
+    /**
+     * Hazelcast instance service.
+     */
+    @Autowired
+    HazelcastInstanceService hazelcastInstanceService
+
+    /**
+     * Name of the hazelcast instance that should contain the map.
+     */
+    String instanceName
+
+    /**
+     * Name of the hazelcast list.
+     */
+    String listName
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    List<T> getObject() throws Exception {
+        return hazelcastInstanceService.getInstance(instanceName).getList(listName)
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    Class<?> getObjectType() {
+        return List.class
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    boolean isSingleton() {
+        return true
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void afterPropertiesSet() throws Exception {
+        if (!listName) {
+            throw new IllegalArgumentException('name of the Hazelcast list may not be null or empty')
+        }
+
+        if (!instanceName) {
+            throw new IllegalArgumentException('name of the hazelcast instance may not be null or empty')
+        }
+    }
+}
