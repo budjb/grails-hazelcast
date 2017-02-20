2bc6421c396a417cde9f05ffcb23560a5293583f
aaron-brown <aaron@aarocode.com>
2017-02-20 12:37:11 -0600

    HazelcastLockFactoryBean.
    Updated tests to prevent side-effects. Also added the test
    to validate that shutdownInstance() behaves as expected (was
    originally added in #1).

---
+++ src/main/groovy/com/budjb/hazelcast/beans/HazelcastLockFactoryBean.groovy
@@ -0,0 +1,67 @@
+package com.budjb.hazelcast.beans
+
+import com.budjb.hazelcast.HazelcastInstanceService
+import org.springframework.beans.factory.FactoryBean
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Autowired
+
+import java.util.concurrent.locks.Lock
+
+/**
+ * A {@link FactoryBean} that exposes a distributed Hazelcast lock as a Spring bean.
+ */
+class HazelcastLockFactoryBean implements FactoryBean<Lock>, InitializingBean {
+    /**
+     * Hazelcast instance service.
+     */
+    @Autowired
+    HazelcastInstanceService hazelcastInstanceService
+
+    /**
+     * Name of the hazelcast instance that should contain the map.
+     */
+    String instanceName
+
+    /**
+     * Name of the hazelcast lock.
+     */
+    String lockName
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    Lock getObject() throws Exception {
+        return hazelcastInstanceService.getInstance(instanceName).getLock(lockName)
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    Class<?> getObjectType() {
+        return Lock.class
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    boolean isSingleton() {
+        return true
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void afterPropertiesSet() throws Exception {
+        if (!lockName) {
+            throw new IllegalArgumentException('name of the Hazelcast lock may not be null or empty')
+        }
+
+        if (!instanceName) {
+            throw new IllegalArgumentException('name of the hazelcast instance may not be null or empty')
+        }
+    }
+}
---
+++ src/test/groovy/com/budjb/hazelcast/beans/HazelcastLocktFactoryBeanSpec.groovy
@@ -0,0 +1,60 @@
+package com.budjb.hazelcast.beans
+
+import com.budjb.hazelcast.HazelcastInstanceService
+import com.hazelcast.config.Config
+import com.hazelcast.core.Hazelcast
+import spock.lang.Specification
+
+import java.util.concurrent.locks.Lock
+
+class HazelcastLocktFactoryBeanSpec extends Specification {
+    void 'When a HazelcastLockFactoryBean is created but is missing the instanceName, an IllegalArgumentException is thrown'() {
+        setup:
+        HazelcastLockFactoryBean factoryBean = new HazelcastLockFactoryBean()
+        factoryBean.lockName = 'foo'
+
+        when:
+        factoryBean.afterPropertiesSet()
+
+        then:
+        thrown IllegalArgumentException
+    }
+
+    void 'When a HazelcastLockFactoryBean is created but is missing the lockName, an IllegalArgumentException is thrown'() {
+        setup:
+        HazelcastLockFactoryBean factoryBean = new HazelcastLockFactoryBean()
+        factoryBean.instanceName = 'foo'
+
+        when:
+        factoryBean.afterPropertiesSet()
+
+        then:
+        thrown IllegalArgumentException
+    }
+
+    void 'When a HazelcastLockFactoryBean is created, it returns the correct Hazelcast lock'() {
+        setup:
+        Config config = new Config()
+        config.instanceName = 'tmp'
+        config.networkConfig.join.multicastConfig.enabled = false
+
+        HazelcastInstanceService hazelcastInstanceService = new HazelcastInstanceService([])
+        hazelcastInstanceService.createInstance(config)
+
+        HazelcastLockFactoryBean factoryBean = new HazelcastLockFactoryBean()
+        factoryBean.hazelcastInstanceService = hazelcastInstanceService
+        factoryBean.instanceName = 'tmp'
+        factoryBean.lockName = 'foo'
+        factoryBean.afterPropertiesSet()
+
+        when:
+        Object object = factoryBean.getObject()
+
+        then:
+        object instanceof Lock
+        object == Hazelcast.getHazelcastInstanceByName('tmp').getLock('foo')
+
+        cleanup:
+        hazelcastInstanceService.shutdownInstance(factoryBean.instanceName)
+    }
+}
