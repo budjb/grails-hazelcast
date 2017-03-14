f6e994c2318de97aed60fe434de880c545114484
Bud Byrd <bud.byrd@gmail.com>
2017-03-14 10:05:03 -0500

    Update developers.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.org/budjb/grails-hazelcast.svg?branch=master)](https://travis-ci.org/budjb/grails-hazelcast)
+[![Build Status](http://img.shields.io/travis/budjb/grails-hazelcast.svg?branch=master)](https://travis-ci.org/budjb/grails-hazelcast)
 [![License](http://img.shields.io/:license-apache-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0.html)
 
 # Grails Hazelcast Plugin
--- src/main/groovy/org/grails/plugins/hazelcast/HazelcastGrailsPlugin.groovy
+++ src/main/groovy/org/grails/plugins/hazelcast/HazelcastGrailsPlugin.groovy
@@ -6,22 +6,22 @@ class HazelcastGrailsPlugin extends Plugin {
     /**
      * The version or versions of Grails the plugin is designed for.
      */
-    def grailsVersion = "3.1.9 > *"
+    def grailsVersion = '3.1.9 > *'
 
     /**
      * Headline display name of the plugin.
      */
-    def title = "Grails Hazelcast Plugin"
+    def title = 'Grails Hazelcast Plugin'
 
     /**
      * Plugin author.
      */
-    def author = "Anil Manthina"
+    def author = 'Anil Manthina'
 
     /**
      * Author email address.
      */
-    def authorEmail = "anil.manthina@gmail.com"
+    def authorEmail = 'anil.manthina@gmail.com'
 
     /**
      * Plugin description.
@@ -31,29 +31,30 @@ class HazelcastGrailsPlugin extends Plugin {
     /**
      * URL to the plugin's documentation.
      */
-    def documentation = "https://budjb.github.io/grails-hazelcast/latest"
+    def documentation = 'https://budjb.github.io/grails-hazelcast/latest'
 
     /**
      * Plugin license.
      */
-    def license = "APACHE"
+    def license = 'APACHE'
 
     /**
      * Additional developers.
      */
     def developers = [
-        [name: "Bud Byrd", email: "bud.byrd@gmail.com"]
+        [name: 'Bud Byrd', email: 'bud.byrd@gmail.com'],
+        [name: 'Aaron Brown', email: 'aaron@aarocode.com']
     ]
 
     /**
      * Location of the plugin's issue tracker.
      */
-    def issueManagement = [system: "GITHUB", url: "https://github.com/budjb/grails-hazelcast/issues"]
+    def issueManagement = [system: 'GITHUB', url: 'https://github.com/budjb/grails-hazelcast/issues']
 
     /**
      * Online location of the plugin's browseable source code.
      */
-    def scm = [url: "https://github.com/budjb/grails-hazelcast"]
+    def scm = [url: 'https://github.com/budjb/grails-hazelcast']
 
     /**
      * Spring bean creation.
